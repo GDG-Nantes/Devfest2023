3d7810bffe0fee5b0d4fdf106a34e916334e8fd0
Arthur <armaury@bouyguestelecom.fr>
2023-09-28 00:07:15 +0200

    liens appli mobile + renvoyer billets

--- src/components/home/jumbo.tsx
+++ src/components/home/jumbo.tsx
@@ -1,11 +1,12 @@
-import { Button, Stack, Typography } from "@mui/material";
+import {Box, Button, IconButton, Modal, Paper, Stack, Typography} from "@mui/material";
 import React from "react";
-import { useTranslation } from "react-i18next";
-import { MyLink } from "../../helpers/links";
-import {LocalDrink, ShoppingBag} from "@mui/icons-material";
+import {useTranslation} from "react-i18next";
+import {MyLink} from "../../helpers/links";
+import {Android, Apple, LocalDrink, ShoppingBag} from "@mui/icons-material";
 
 export const HomeJumbo = () => {
-  const { t } = useTranslation("translation", {
+  const [modalOpen, setModalOpen] = React.useState(false)
+  const {t} = useTranslation("translation", {
     keyPrefix: "pages.home.jumbo",
   });
 
@@ -15,8 +16,8 @@ export const HomeJumbo = () => {
         <img
           alt="logo"
           src="/images/logo_transparent.svg"
-          height="300"
-          width="600"
+          height="200"
+          width="400"
         />
       </div>
 
@@ -26,7 +27,7 @@ export const HomeJumbo = () => {
       <Typography
         variant="h2"
         textAlign="center"
-        style={{ marginTop: "10px", marginBottom: "25px" }}
+        style={{marginTop: "10px", marginBottom: "25px"}}
       >
         Code, Dev, et... Action !
       </Typography>
@@ -58,19 +59,63 @@ export const HomeJumbo = () => {
           {/*>*/}
           {/*  Devenir Sponsor*/}
           {/*</Button>*/}
+          {/*<Button*/}
+          {/*  color="secondary"*/}
+          {/*  variant="contained"*/}
+          {/*  href="https://www.billetweb.fr/devfest-nantes"*/}
+          {/*  aria-label="Billetterie"*/}
+          {/*>*/}
+          {/*  Billetterie*/}
+          {/*</Button> */}
           <Button
             color="secondary"
             variant="contained"
-            href="https://www.billetweb.fr/devfest-nantes"
-            aria-label="Billetterie"
+            href="https://billetterie.gdgnantes.com"
+            aria-label="Send tickets"
+            target={"_blank"}
           >
-            Billetterie
+            {t('resend-tickets')}
           </Button>
+          <Button
+            color="secondary"
+            variant="outlined"
+            onClick={() => setModalOpen(true)}
+            aria-label="Send tickets"
+          >
+            {t('get-app')}
+          </Button>
+
         </Stack>
         <Stack direction="row" spacing={3} justifyContent={"center"}>
           <Button href="/our-values" color="secondary">{t('bring-ecocup')}</Button>
         </Stack>
       </Stack>
+      <Modal open={modalOpen} onClose={() => setModalOpen(false)} className={"modal-apps"}>
+        <Paper className={"modal-content"}>
+          <Box className={"modal-body"}>
+            <Stack spacing={[5,5]} direction={["column", "row"]}>
+              <Button
+                color="secondary"
+                variant="contained"
+                href="https://apps.apple.com/fr/app/devfest-nantes/id6443489706"
+                aria-label="iOS app"
+                startIcon={<Apple/>}
+              >
+                iOS
+              </Button>
+              <Button
+                color="secondary"
+                variant="contained"
+                href="https://play.google.com/store/apps/details?id=com.gdgnantes.devfest.androidapp&pli=1"
+                aria-label="Android app"
+                startIcon={<Android/>}
+              >
+                Android
+              </Button>
+            </Stack>
+          </Box>
+        </Paper>
+      </Modal>
     </>
   );
 };
